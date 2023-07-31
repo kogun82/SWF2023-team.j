4bdbacdf4e7d0520ea0ae717a965b74dca7d3990
wangu <wangu@BOOK-9BO5PPM84A>
2023-07-31 16:03:38 +0900

    최초 생성

---
+++ geno.block.web/src/main/java/org/team/j/geno/block/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package org.team.j.geno.block.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wangho
+ *
+ */
+@Controller
+public class UserController {
+
+	/**
+	 * @Author          : wangho
+	 * @Date            : 2023. 7. 31.
+	 * @Description     : 
+	 * @ModifiedHistory : 
+	 */
+	@RequestMapping(value = "/")
+	public String main() {
+		
+		return null;
+	}
+}
